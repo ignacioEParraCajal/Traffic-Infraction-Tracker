d050b576eb82d56a3a8a6db80417122f04f77f87
ignacioEParraCajal <ignacioemmanuelparracajal@gmail.com>
2024-06-09 02:55:53 -0300

    Move SECRET_KEY and SQLALCHEMY_DATABASE_URI values to constants

--- src/conf.py
+++ src/conf.py
@@ -1,10 +1,13 @@
 import os
 import secrets
 
+SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
+
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///traffic_infraction_tracker.db'
+    SECRET_KEY = SECRET_KEY
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
