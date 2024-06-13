def3c6bab119da5557a9dbb2ede67afc1a3db2c5
ignacioEParraCajal <ignacioemmanuelparracajal@gmail.com>
2024-06-13 19:59:37 -0300

    Update creation app logic and configuration

--- src/conf.py
+++ src/conf.py
@@ -1,14 +1,7 @@
 import os
-import secrets
-
-SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
-DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
 
 
 class Config:
-    SECRET_KEY = SECRET_KEY
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-config = Config
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
